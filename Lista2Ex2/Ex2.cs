160f3183c0fd48486a2bf7ce327ba28bd47b8fa6
Letícia Lopes Fogaça <82903905+Lazully@users.noreply.github.com>
2023-04-03 19:42:43 -0300

    Update Ex2.cs

--- Lista2Ex2/Ex2.cs
+++ Lista2Ex2/Ex2.cs
@@ -1,38 +1,33 @@
-﻿//Faça um programa que leia um número n e mostre na tela os n primeiros números pares e depois
+//Faça um programa que leia um número n e mostre na tela os n primeiros números pares e depois
 //os n primeiros números ímpares.
 
 using System;
 
-namespace Lista2Ex2
+namespace Ex2
 {
     class Ex2
     {
         static void Main(string[] args)
         {
-            int n, seguintePar = 0, seguinteImpar = 0;
+            int n, seguintePar = 0, seguinteImpar = 1;
 
             Console.WriteLine($"====== Mostra Seguintes Par e Impar ======");
 
             Console.WriteLine($"{Environment.NewLine}Digite um número: ");
             n = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = n; i < (n + n); i++)
+            Console.WriteLine($"{Environment.NewLine}------------------------------------{Environment.NewLine}");
+
+            for (int i = 0; i < n; i++)
             {
-                if (n % 2 == 0)
-                {
-                    seguintePar = n;
-                    seguintePar +=2;
-                }
-                else
-                {
-                    if (n % 2 == 1)
-                    {
-                        seguinteImpar = n;
-                        seguinteImpar +=2;
-                    }
-                }
-                Console.WriteLine($"{Environment.NewLine}Primeiros números pares: {seguintePar}");
-                Console.WriteLine($"{Environment.NewLine}Primeiros números ímpares: {seguinteImpar}");
+                Console.WriteLine($"{Environment.NewLine}Par: {seguintePar}");
+                seguintePar += 2;
+            }
+            Console.WriteLine($"{Environment.NewLine}------------------------------------{Environment.NewLine}");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{Environment.NewLine}Ímpar: {seguinteImpar}");
+                seguinteImpar += 2;
             }
         }
     }
