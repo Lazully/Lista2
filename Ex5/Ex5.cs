daccbb27f928adea359746a3b8f1992e7b1c50e0
Letícia Lopes Fogaça <82903905+Lazully@users.noreply.github.com>
2023-04-03 20:00:33 -0300

    Update Ex5.cs

--- Ex5/Ex5.cs
+++ Ex5/Ex5.cs
@@ -1,4 +1,4 @@
-﻿//Escreva um programa que lê 15 valores reais, encontra o maior e o menor deles e mostra o
+//Escreva um programa que lê 15 valores reais, encontra o maior e o menor deles e mostra o
 //resultados.
 
 using System;
@@ -9,7 +9,31 @@ namespace Ex5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("============ Encontra Menor Valor ============");
+
+            MaiorMenor();
+        }
+
+        public static void MaiorMenor()
+        {
+            int maior=1, menor=100000000, numEntrada;
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{Environment.NewLine}Digite um número: ");
+                numEntrada = Convert.ToInt32(Console.ReadLine());
+
+                if(numEntrada > maior)
+                {
+                    maior = numEntrada;
+                }
+
+                if(numEntrada < menor)
+                {
+                    menor = numEntrada;
+                }
+            }
+            Console.WriteLine($"{Environment.NewLine}Maior: {maior} {Environment.NewLine}Menor: {menor}");
         }
     }
 }
